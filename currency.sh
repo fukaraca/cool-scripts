f8edd103cb342aea179aba64f87d49d8941cb286
fukaraca <fukaraca@hotmail.com>
2022-07-02 23:26:14 +0300

    currency

--- currency.sh
+++ currency.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-CURRAPI=https://api.exchangerate.host/latest
+CURRAPI=https://api.exchangerate.host/latest=?places=3
 
 echo "Enter base currency"
 read base
 echo "Enter target currency"
 read currency
+echo "Enter amount"
+read amount
 
-curl -X 'GET' $CURRAPI\?base\=$base\&symbols\=$currency 
-echo ""
+curl -X 'GET' $CURRAPI\&base\=$base\&symbols\=$currency\&amount=$amount | printf "\n$amount amount of $base : $(grep -Po "\brates\":{\"$currency\":\K\d+.\d+") $currency \n"
+
 exit
